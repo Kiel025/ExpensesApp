b28360f08a99e8666f9428755663dd5f1fe24b92
kiel025 <kiel02505@gmail.com>
2023-07-23 19:33:13 -0300

    displaying custom items

--- lib/main.dart
+++ lib/main.dart
@@ -51,11 +51,23 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Column(
-            children: _transactions
-                .map((tr) => Card(
-                      child: Text(tr.title),
-                    ))
-                .toList(),
+            children: _transactions.map((tr) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      child: Text(tr.value.toString()),
+                    ),
+                    Column(
+                      children: [
+                        Text(tr.title),
+                        Text(tr.date.toString()),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
           ),
         ],
       ),
