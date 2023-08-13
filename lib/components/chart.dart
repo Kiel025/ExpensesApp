12c2fa76e9e5ba9f877cc2707a78bfb6d716903c
kiel025 <kiel02505@gmail.com>
2023-08-13 17:32:32 -0300

    Updating ChartWidget

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -26,21 +26,11 @@ class Chart extends StatelessWidget {
         }
       }
 
-      // final totalSum = recentTransactions
-      //     .where((element) =>
-      //         element.date.day == weekDay.day &&
-      //         element.date.month == weekDay.month &&
-      //         element.date.year == weekDay.year)
-      //     .map((e) => e.value)
-      //     .toList();
-      // .reduce((value, [element = 0]) => value + element)
-      // .toDouble();
-
       return {
         'day': DateFormat.E().format(weekDay)[0],
         'value': totalSum,
       };
-    });
+    }).reversed.toList();
   }
 
   double get _weekTotalValue {
